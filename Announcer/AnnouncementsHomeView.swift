735c7423b2ac12b1290bf6458a6e0f00b7e02f35
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-01-04 20:34:46 +0800

    Move navigation link arrow to the top

--- Announcer/AnnouncementsHomeView.swift
+++ Announcer/AnnouncementsHomeView.swift
@@ -46,42 +46,45 @@ struct AnnouncementsHomeView: View {
     var body: some View {
         List {
             ForEach($posts, id: \.title) { $post in
-                NavigationLink {
-                    AnnouncementDetailView(post: $post)
-                        .onChange(of: post) { changedPost in
-                            PostManager.savePost(post: changedPost)
+                VStack(alignment: .leading) {
+                    HStack {
+                        if post.pinned {
+                            Image(systemName: "pin.fill")
+                                .foregroundColor(.accentColor)
+                                .font(.footnote)
                         }
-                } label: {
-                    VStack(alignment: .leading) {
-                        HStack {
-                            if post.pinned {
-                                Image(systemName: "pin.fill")
-                                    .foregroundColor(.accentColor)
-                                    .font(.footnote)
-                                    .padding(.trailing, -4)
-                            }
-                            Text(post.title)
-                                .lineLimit(2)
+                        Text(post.title)
+                            .lineLimit(2)
+
+                        Spacer()
+                        NavigationLink {
+                            AnnouncementDetailView(post: $post)
+                                .onChange(of: post) { changedPost in
+                                    PostManager.savePost(post: changedPost)
+                                }
+                        } label: {
+                            EmptyView()
                         }
+                        .frame(width: 0, height: 0)
+                    }
+                    .padding(.bottom, 0.5)
+
+                    // preview
+                    Text(post.content.replacingOccurrences(of: "\n\n", with: "\n"))
+                        .opacity(post.read ? 0.3 : 0.7)
+                        .lineLimit(3)
                         .padding(.bottom, 0.5)
 
-                        // preview
-                        Text(post.content.replacingOccurrences(of: "\n\n", with: "\n"))
-                            .opacity(post.read ? 0.3 : 0.7)
-                            .lineLimit(3)
-                            .padding(.bottom, 0.5)
-
-                        // post and reminder
-                        HStack {
-                            Image(systemName: "timer")
-                            Text("03 Jan 2023")
-                                .padding(.trailing, 10)
-                            Image(systemName: "alarm")
-                            Text("8h")
-                        }
-                        .font(.footnote)
-                        .opacity(post.read ? 0.5 : 0.6)
+                    // post and reminder
+                    HStack {
+                        Image(systemName: "timer")
+                        Text("03 Jan 2023")
+                            .padding(.trailing, 10)
+                        Image(systemName: "alarm")
+                        Text("8h")
                     }
+                    .font(.footnote)
+                    .opacity(post.read ? 0.5 : 0.6)
                 }
                 .swipeActions(edge: .leading, allowsFullSwipe: true) {
                     Button {
