8d651428a60317f5c04edeedf6596dc9b31cb8f5
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-01-06 22:12:52 +0800

    Implement search (does not work with categories)

--- Announcer/Data/PostManager.swift
+++ Announcer/Data/PostManager.swift
@@ -85,3 +85,17 @@ enum PostManager {
 extension String {
     static let userCategories = "userCategories"
 }
+
+let placeholderTextShort = "Lorem ipsum dolor sit amet"
+let placeholderTextLong = """
+Dear Students,
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
+ut labore et dolore magna aliqua. Turpis egestas pretium aenean pharetra. Orci eu lobortis.
+
+elementum nibh tellus molestie. Vulputate dignissim suspendisse in est. Vel pharetra vel \
+turpis nunc. Malesuada nunc vel risus commodo. Nisi vitae suscipit tellus mauris.
+
+Posuere orbi leo urna molestie at elementum eu. Urna duis convallis convallis tellus. Urna molestie \
+at elementum eu. Nunc sed blandit libero volutpat.
+"""
--- Announcer/UI/Pages/AnnouncementsHomeView.swift
+++ Announcer/UI/Pages/AnnouncementsHomeView.swift
@@ -7,20 +7,6 @@
 
 import SwiftUI
 
-let placeholderTextShort = "Lorem ipsum dolor sit amet"
-let placeholderTextLong = """
-Dear Students,
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt \
-ut labore et dolore magna aliqua. Turpis egestas pretium aenean pharetra. Orci eu lobortis.
-
-elementum nibh tellus molestie. Vulputate dignissim suspendisse in est. Vel pharetra vel \
-turpis nunc. Malesuada nunc vel risus commodo. Nisi vitae suscipit tellus mauris.
-
-Posuere orbi leo urna molestie at elementum eu. Urna duis convallis convallis tellus. Urna molestie \
-at elementum eu. Nunc sed blandit libero volutpat.
-"""
-
 struct AnnouncementsHomeView: View {
     @State
     var posts: [Post] = []
@@ -51,7 +37,11 @@ struct AnnouncementsHomeView: View {
 
     var content: some View {
         List {
-            ForEach($posts, id: \.title) { $post in
+            ForEach($posts.filter({
+                searchString.isEmpty ||
+                $0.wrappedValue.title.lowercased().contains(formattedSearchString()) ||
+                $0.wrappedValue.content.lowercased().contains(formattedSearchString())
+            }), id: \.wrappedValue.title) { $post in
                 PostPreviewView(post: $post, posts: $posts)
             }
         }
@@ -85,6 +75,11 @@ struct AnnouncementsHomeView: View {
                 self.posts = posts
             }
         }
+    }
+
+    func formattedSearchString() -> String {
+        // get rid of filters in the search bar
+        searchString.lowercased()
     }
 
     func numberOfFilters(pinnedTo range: Range<Int>? = nil) -> Int {
