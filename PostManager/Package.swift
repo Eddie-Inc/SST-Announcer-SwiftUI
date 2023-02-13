3440dd862ca3295d73509c50b6495dd33fa860b0
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-02-13 20:01:05 +0800

    Fix dependency issues

--- PostManager/Package.swift
+++ PostManager/Package.swift
@@ -18,13 +18,18 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.0.3")),
+        .package(url: "https://github.com/nmdias/FeedKit.git", branch: "master")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "PostManager",
-            dependencies: []),
+            dependencies: [
+                .product(name: "OrderedCollections", package: "swift-collections"),
+                .product(name: "FeedKit", package: "FeedKit")
+            ]),
         .testTarget(
             name: "PostManagerTests",
             dependencies: ["PostManager"])
