98c5e9ccf9e0451305925acc2219cdada0678660
KaiTheRedNinja <88234730+KaiTheRedNinja@users.noreply.github.com>
2023-03-07 22:03:19 +0800

    Implement small widget

--- Timetable_Announcer/UI/TimeTableSmall.swift
+++ Timetable_Announcer/UI/TimeTableSmall.swift
@@ -16,11 +16,60 @@ struct TimeTableSmall: TimeTableProtocol {
     @Updating
     var today: Date
 
+    // FOR DEBUG PURPOSES ONLY
+    @State
+    var message: String = ""
+
+    init(today: Date) {
+        self._today = <-today
+        ScheduleManager.default.fetchSchedule()
+    }
+
     var body: some View {
-        VStack {
-            Text(today.formatted(date: .abbreviated, time: .shortened))
-            Text("Small")
+        ZStack {
+            if manager.currentSchedule != nil {
+                VStack(alignment: .leading) {
+                    Text("W\(manager.schedule.currentWeek), \(today.weekday.rawValue.firstLetterUppercase)")
+                    ForEach(0..<3) { index in
+                        HStack {
+                            (todaySubjects[index].displayColor ?? .accentColor)
+                                .frame(width: 6)
+                                .cornerRadius(3)
+                                .padding(.trailing, -4)
+                            ZStack(alignment: .leading) {
+                                (todaySubjects[index].displayColor ?? .accentColor)
+                                    .cornerRadius(4)
+                                    .opacity(0.3)
+                                HStack {
+                                    Text(todaySubjects[index].displayName?.description ?? "Unnamed")
+                                        .font(.caption)
+                                    Spacer()
+                                    Text(todaySubjects[index].durationFormatted)
+                                        .font(.caption2)
+                                }
+                                .padding(.horizontal, 5)
+                            }
+                        }
+                        .padding(.vertical, -2)
+                    }
+                    HStack {
+                        ForEach([Color.red, Color.blue, Color.green], id: \.self) { color in
+                            color
+                                .cornerRadius(3)
+                                .frame(width: 6, height: 6)
+                                .padding(.horizontal, -2)
+                        }
+                        Text("3 More")
+                            .font(.caption)
+                    }
+                    .padding(.leading, 2)
+                    .frame(height: 10)
+                }
+            } else {
+                Text("No Schedule Found")
+            }
         }
+        .padding(12)
     }
 }
 
